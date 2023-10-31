eabf9c44abbed82362cc965ae6373a47059fc324
Alumno <Alumno@equipo-01>
2023-10-31 13:30:24 -0600

    intento6

--- src/myserversocket/MyServerSocket.java
+++ src/myserversocket/MyServerSocket.java
@@ -3,67 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package myserversocket;
-
-import java.io.BufferedReader;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Serializable;
-
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author gerdoc
- */
-public class MyServerSocket implements Serializable
-{
+public class MyServerSocket {
+    private static final int PORT = 12345;
+    private static CopyOnWriteArrayList<MiNegocio> clients = new CopyOnWriteArrayList<>();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-        PrintWriter printWriter = null;
-        BufferedReader bufferedReader = null;
-        int port = 9999;
-        String msgRead = null;
-        try 
-        {    
-            serverSocket = new ServerSocket( port );
-            Logger.getLogger(MyServerSocket.class.getName( ) ).log(Level.INFO, "Waiting..." );
-            socket = serverSocket.accept( );
-            printWriter = new PrintWriter(socket.getOutputStream(), true);
-            bufferedReader = new BufferedReader(new InputStreamReader( socket.getInputStream()));
-            msgRead = bufferedReader.readLine( );
-            Logger.getLogger(MyServerSocket.class.getName( ) ).log(Level.INFO, "--------------------" );
-            Logger.getLogger(MyServerSocket.class.getName( ) ).log(Level.INFO, msgRead );
-            Logger.getLogger(MyServerSocket.class.getName( ) ).log(Level.INFO, "--------------------" );
-            if ("hola gerdoc".equals(msgRead )) 
-            {
-                printWriter.println("Hola mundo");
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Servidor conectado, conectate a: " + PORT);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Usuario extra");
+                MiNegocio client = new MiNegocio(clientSocket);
+                clients.add(client);
+                client.start();
             }
-            else 
-            {
-                printWriter.println("Hola");
-            }
-            
-            bufferedReader.close( );
-            printWriter.close( );
-            socket.close( );
-            serverSocket.close( );
-            Logger.getLogger(MyServerSocket.class.getName( ) ).log(Level.INFO, "End" );
-        } 
-        catch (IOException ex) 
-        {
-            Logger.getLogger(MyServerSocket.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    
+
+    public static void broadcastMessage(String message, MiNegocio sender) {
+        for (MiNegocio client : clients) {
+            if (client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
 }
